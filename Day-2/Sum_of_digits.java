75d3740d4c78f2150ad923d7a7c6c152ad7e091e
akshitk0001 <akshitk0003@gmail.com>
2026-06-05 11:22:26 +0530

    Added Day 2 Java assignments

---
+++ Day-2/Sum_of_digits.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Sum_of_digits {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        
+        int sum = 0;
+        while(n > 0) {
+            sum = sum + (n % 10);
+            n = n / 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+    }
+}
