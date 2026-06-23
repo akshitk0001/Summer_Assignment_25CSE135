449f2836612fefc86acb51205f6e12c2d57e821e
akshitk0001 <akshitk0003@gmail.com>
2026-06-23 07:02:26 +0530

    Added Day 23 Java assignments

---
+++ Day-23/Anagram_check.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Anagram_check {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+            if(str1.length() != str2.length()) {
+            System.out.println("Not Anagrams");
+        }
+        else {
+            char arr1[] = str1.toCharArray();
+            char arr2[] = str2.toCharArray();
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            if(Arrays.equals(arr1, arr2)) {
+                System.out.println("Anagram Strings");
+            }
+            else {
+                System.out.println("Not Anagrams");
+            }
+        }
+    }
+}
---
+++ Day-23/First_repeating_character.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class First_repeating_character {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        boolean found = false;
+        for(int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+            if(count > 1) {
+                System.out.println("First Repeating Character = " + str.charAt(i));
+                found = true;
+                break;
+            }
+        }
+        if(found == false) {
+            System.out.println("No repeating character found.");
+        }
+    }
+}
